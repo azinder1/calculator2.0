e0801c5c4e8eb1ff156cd2048dea5b7c18db8180
Adam Zinder and Erik Killops <adam@>
2016-10-19 14:47:28 -0700

    create calculate function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+//Business Logic:
+
 var add = function(number1, number2) {
 	return number1 + number2;
 };
 
 var subtract = function(number1, number2) {
+
 	return number1 - number2;
 };
 
@@ -14,11 +17,30 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var calculate = function(number1, number2, operation) {
+		switch (operation) {
+			case add:
+				return number1 + number2;
+			case subtract:
+				return number1 - number2;
+			case multiply:
+				return number1 * number2;
+			case divide:
+				return number1 / number2;
+			default:
+				break
+		}
+}
+
+//User interface logic:
+
 $(document).ready(function(){
-	$("form#add").submit(function(event) {
-		var number1 = parseInt($("input#add1").val());
-		var number2 = parseInt($("input#add2").val());
-		var result = add(number1, number2);
+	$("form#calculator").submit(function(event) {
+		var number1 = parseInt($("input#number1").val());
+		var number2 = parseInt($("input#number2").val());
+		var operation;
+		if
+
 		$("#output").text(result);
 		event.preventDefault();
 	});
